5f1c647d94dacccb56584a4cc2e6dc82653a3936
Valeria Ortiz de la Tabla <130620001+mariavaleriaot@users.noreply.github.com>
2024-05-30 20:29:56 -0400

    add javaProject

---
+++ EvaluacionIntegradora6/src/main/java/cl/bootcamp/proyectoWeb/EvaluacionIntegradora6Application.java
@@ -0,0 +1,13 @@
+package cl.bootcamp.proyectoWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvaluacionIntegradora6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvaluacionIntegradora6Application.class, args);
+	}
+
+}
---
+++ EvaluacionIntegradora6/src/test/java/cl/bootcamp/proyectoWeb/EvaluacionIntegradora6ApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.bootcamp.proyectoWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvaluacionIntegradora6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
